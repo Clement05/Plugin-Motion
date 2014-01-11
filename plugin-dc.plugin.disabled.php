214d9a761de3b88c7e23224dbd1fd20b45d8f361
Clement Girard <clement.girard4@gmail.com>
2014-01-11 05:08:10 +0100

    stop

--- plugin-dc.plugin.disabled.php
+++ plugin-dc.plugin.disabled.php
@@ -50,7 +50,7 @@ function dc_plugin_setting_page(){
 						<br/>
 							<p style="float: left;"><button type="submit" class="btn"><? echo $button; ?></button>
 							</p>
-							<p style="float: right;"><a class="btn" href="setting.php?section=PluginDC&save=true"<i class="icon-check icon-black"></i> Activer la surveillance</a><a class="btn" href="setting.php?section=PluginDC?reset="true"><i class="icon-remove icon-black"></i> Annuler</a>
+							<p style="float: right;"><a class="btn" href="setting.php?section=PluginDC&save=true"<i class="icon-check icon-black"></i> Activer la surveillance</a><a class="btn" href="setting.php?section=PluginDC"><i class="icon-remove icon-black"></i> Annuler</a>
 							</p>
 					</fieldset>
 					<br/>
@@ -87,8 +87,8 @@ function dc_plugin_setting_page(){
 					}
 					if (isset($_GET['reset'])){ // On les données envoyées
 					  if ($_GET['reset']=="true"){
-						exec('sudo -u www-data /var/www/yana-server/plugins/Plugin-DetectionControl/stop.sh'); exit;
-						}
+$contents = file_get_contents('/var/www/yana-server/plugins/Plugin-DetectionControl/stop.sh');
+echo shell_exec($contents);						}
 					}
 					
 				}else{ ?>
